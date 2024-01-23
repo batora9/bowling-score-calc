d819f0594cfa715f3522e0a2c7d96242a5ca297e
syun_m <sm.net_minecraft.hacker@outlook.jp>
2024-01-23 13:31:54 +0900

    make project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main(){
+    cout << " You | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10|Total" << endl;
+    cout << "-----+---+---+---+---+---+---+---+---+---+---+-----" << endl;
+    cout << "     | X | X | X | X | X | X | X | X | X |XXX| 300 " << endl;
+    cout << "     | 30| 60| 90|120|150|180|210|240|270|300| 300 " << endl;
+    return 0;
+}
